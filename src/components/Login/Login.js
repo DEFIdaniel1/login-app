c0e61e7bf7c5b5cfcde3f65338441368e6097dbd
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-04-08 11:54:37 -0600

    useReducer() added for email input
    altering useEffect + useState to useReducer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,47 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: false };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes('@')}
+  }
+  return { value: '', isValid: action.val.includes('@') }
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {value: "",
+    isValid: null}, );
+
+
   //reruns this code only when these elements change and "listen" for keystrokes to update form validity
   useEffect(() => {
     //adding timeout feature to limit requests sent
-    const serverRequest = setTimeout(() => { 
-      console.log("first one");
-      setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
-    }, 500);//500 milliseconds before "completing"
+    const serverRequest = setTimeout(() => {
+      setFormIsValid(emailState.isValid && enteredPassword.trim().length > 6);
+    }, 500); //500 milliseconds before "completing"
 
-    return () => { //this is the CLEANUP function
-      console.log(serverRequest);
+    return () => {
+      //this is the CLEANUP function. gets rid of each keystroke request before timer
       clearTimeout(serverRequest);
-    }
-
-  }, [setFormIsValid, enteredEmail, enteredPassword]);
+    };
+  }, [setFormIsValid, emailState.value, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value});
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
     setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes("@")
+      event.target.value.trim().length > 6 && emailState.isValid
     );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    dispatchEmail({type: "INPUT_BLUR"});
   };
 
   const validatePasswordHandler = () => {
@@ -50,7 +59,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -58,14 +67,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
