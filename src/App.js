3d29119eb3f4dff9e5fa8e693b3ababdd641ab4d
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-04-11 16:04:32 -0600

    added central context and central state management
    all login states now all incorporated directly into the auth-context file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,38 +6,16 @@ import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './components/Store/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  //checks if user is logged in and will first relog them in again first
-  useEffect(() => {
-    const storedLogin = localStorage.getItem("isLoggedIn");
-    if (storedLogin === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-  
-  const loginHandler = (email, password) => {
-    //stores login status in the localstorage
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    //ensure login status is set to false
-    localStorage.setItem("isLoggedIn", "0");
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
 
   return (
-    <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn,
-      onLogout: logoutHandler}}>
-      <MainHeader onLogout={logoutHandler} />
+    <>
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </>
   );
 }
 
--- src/components/Store/auth-context.js
+++ src/components/Store/auth-context.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //checks if user is logged in and will first relog them in again first
+  useEffect(() => {
+    const storedLogin = localStorage.getItem("isLoggedIn");
+    if (storedLogin === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  
+  const loginHandler = (email, password) => {
+    //stores login status in the localstorage
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    //ensure login status is set to false
+    localStorage.setItem("isLoggedIn", "0");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{
+          isLoggedIn: isLoggedIn, 
+          onLogout: logoutHandler, 
+          onLogin: loginHandler}}>
+              {props.children}
+    </AuthContext.Provider>
+  )
+}
 
 export default AuthContext;
